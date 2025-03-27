9377bea89bc5a7ce84528af06c12a4ac8f40b9b1
Matt Hannigan <matthannigan@users.noreply.github.com>
2025-03-26 23:33:48 -0400

    chore(docs): remove old docs

--- docs/components/commands.sh
+++
@@ -1,6 +0,0 @@
-# Step 1: Initialize shadcn/ui
-npx shadcn@2.3.0 init
-
-# Step 2: Copy basic shadcn/ui components
-npx shadcn@2.3.0 add alert avatar button calendar card checkbox dialog dropdown-menu form input label navigation-menu popover radio-group select switch table tabs textarea typography
-
