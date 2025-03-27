40ea260820f1eca64b12fdf4771e81d89e302111
Matt Hannigan <matthannigan@users.noreply.github.com>
2025-03-27 00:23:19 -0400

    fix: update import statements for new components dir structure

--- src/app/page-alt.tsx
+++ src/app/page-alt.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
-import { Flex, Grid } from '@/components/ui/layout';
+import { Flex, Grid } from '@/components/layout/layout';
 
 export default function Home() {
   return (
